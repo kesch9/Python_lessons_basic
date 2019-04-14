433a868d12be092b153cfb1af3835567348d19db
kesch9 <kashenkovsergej@yandex.ru>
2019-04-14 20:47:06 +0300

    lesson_4 normal complete

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -3,6 +3,8 @@
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+import re
+import random
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -19,7 +21,19 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+pat = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+list_find = []
+s = ''
+for i in line:
+       if i in pat:
+              if s:
+                     list_find.append(s)
+                     s = ''
+       else:
+              s += i
+print(list_find)
 
+print(re.split('[A-Z]+',line))
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +59,20 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+print(list_find)
+print(re.findall('[a-z]{2}([A-Z]+)[A-Z]{2}', line_2))
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+with open('test.txt', 'w+', encoding='UTF-8') as f:
+       for _ in range(2500):
+              c = random.randint(0, 9)
+              f.write(f'{c}')
+       f.seek(0)
+       str_find = f.read()
+digit_list = re.findall('0+|1+|2+|3+|4+|5+|6+|7+|8+|9+', str_find)
+print(max(digit_list, key=len))
