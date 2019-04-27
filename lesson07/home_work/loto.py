8a03a65f116e919e525f9894c4a9392483aea402
KashenkovSergej <kashenkovsergej@yandex.ru>
2019-04-27 16:01:25 +0300

    Lesson 7 complete

--- lesson07/home_work/loto.py
+++ lesson07/home_work/loto.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 == Лото ==
 
@@ -57,3 +56,86 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+import random
+
+
+class Cards:
+
+    def __init__(self, name_cards, list_value):
+        self.name_cards = name_cards
+        self.listValue = list_value
+
+    def __str__(self):
+        list_value_str = ""
+        for row in self.listValue:
+            list_value_str += ' '.join([str(elem) for elem in row])
+            list_value_str += '\n'
+        return f"{self.name_cards}\n" \
+               f"{list_value_str}" \
+               f"--------------------------"
+
+    def del_val(self, val):
+        for row in self.listValue:
+            if val in row:
+                row[row.index(val)] = ' '
+                return True
+        return False
+
+    def win(self):
+        for row in self.listValue:
+            for val in row:
+                if ' ' != val:
+                    return False
+        return True
+
+def gen_cards():
+    cards = []
+    for i in range(3):
+        cards.append([])
+        for j in range(9):
+            cards[i].append(' ')
+    i = 0
+    val = [i for i in range(91)]
+    while ( i < 15):
+        a = random.randint(0, 2)
+        b = random.randint(0, 8)
+        if cards[a][b] == ' ':
+            k = random.randint(0, len(val))
+            cards[a][b] = val[k]
+            val.remove(val[k])
+            i += 1
+    return cards
+
+listComp = gen_cards()
+listUser = gen_cards()
+name = "------ Ваша карточка -----"
+cardsUser = Cards(name, listUser)
+name = "-- Карточка компьютера ---"
+cardsComp = Cards(name, listComp)
+print("---ИГРА НАЧИНАЕТСЯ---")
+keg = [i for i in range(91)]
+
+while(True):
+    ind = random.randint(0, len(keg))
+    print(f"Новый бочонок: {keg[ind]} (осталось {len(keg)-1})")
+    elem = keg[ind]
+    keg.remove(elem)
+    print(cardsUser)
+    print(cardsComp)
+    choise_user = input("Зачеркнуть цифру? (y/n) ")
+    if choise_user == "y":
+        if not cardsUser.del_val(elem):
+            print("Вы проиграли и игра завершена")
+            break
+    else:
+        if cardsUser.del_val(elem):
+            print("Вы проиграли и игра завершена")
+            break
+    cardsComp.del_val(elem)
+    if cardsUser.win():
+        print("Вы выиграли !!!!")
+        break
+
+
+
+
